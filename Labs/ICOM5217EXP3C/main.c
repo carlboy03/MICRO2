457dfd5df310af7c007a5421e5138473ca9098c8
s801082118 <s801082118@MICROWS05.ecenet.ece.uprm.edu>
2016-03-14 23:56:54 -0400

    changed to code from Exp 2 HW and changed port reading to PD2 PD3 to get the rpr220 working

--- Labs/ICOM5217EXP3C/main.c
+++ Labs/ICOM5217EXP3C/main.c
@@ -11,7 +11,7 @@
 #include <stdio.h>
 
 /******************************************************************
- * By: Alan López García and Carlos Fuentes Rosa 3/14/2016
+ * By: Alan López García and Carlos Fuentes Rosa 3/2/2016
  * References:
  * TI, Getting Started with the Tica TM4C132G LaunchPad Workhsop
  * https://e2e.ti.com/support/microcontrollers/tiva_arm/f/908/t/332605
@@ -42,66 +42,77 @@ uint8_t buttonUpPin = GPIO_PIN_3;
 uint8_t buttonDownPin = GPIO_PIN_2;
 uint8_t buttonDebouncePin = GPIO_PIN_6;
 
+#define photosensorTop GPIO_PIN_2
+#define photosensorBottom GPIO_PIN_3
+
 
 volatile char buffer [16];
 volatile int  sizeOfInteger;
 void portAISR(void);
 void portCISR(void);
 void portDISR(void);
+void lineSwitcher();
 
 
+volatile uint8_t currentState;
+volatile int globalCounter;
 
-volatile int globalCounter=-1;
+static char line0[2] = "La";//2
+static char line1[11] = "computadora";//11
+static char line2[2] = "es";//2
+static char line3[3] = "tan";//3
+static char line4[11] = "inteligente";//11
+static char line5[4] = "como";//4
+static char line6[2] = "tu";//2
+static char line7[2] = "lo";//2
+static char line8[5] = "seas.";//5
+static char line9[10] = "-El procer";//10
+static char line10[12] = "Danilo Rojas";//12
+static char line11[2] = "Un";//2
+static char line12[2] = "23";//2
+static char line13[10] = "de febrero";//10
+static char line14[7] = "de 2016";//7
+static char line15[8] = "Linea 16";//8
+static char hello[11] = "hello world";
 
-
+volatile int lineLengthArray[16] = {2,11,2,3,11,4,2,2,5,10,12,2,2,10,7,8};
 
 
 int main(void){
-	SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
+    SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
+	//SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
 
 
 
 
 	GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, RS|E|RW);
 	GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, bit4|bit5|bit6|bit7);
-	GPIOPinTypeGPIOInput(GPIO_PORTA_BASE, buttonUpPin|buttonDownPin);
-	GPIOPinTypeGPIOInput(GPIO_PORTA_BASE, buttonUpPin|buttonDownPin);//Sensors are in portD2-3
-	GPIOPinTypeGPIOInput(GPIO_PORTC_BASE, buttonDebouncePin);
+	GPIOPinTypeGPIOInput(GPIO_PORTD_BASE, photosensorTop | photosensorBottom);
+	//GPIOPinTypeGPIOInput(GPIO_PORTC_BASE, buttonDebouncePin);
 
 
 
 	// Interrupt setup 1
-	GPIOIntDisable(GPIO_PORTA_BASE, buttonUpPin|buttonDownPin);
-	GPIOIntClear(GPIO_PORTA_BASE, buttonUpPin|buttonDownPin);
-	GPIOIntRegister(GPIO_PORTA_BASE, portAISR);
-	GPIOIntTypeSet(GPIO_PORTA_BASE, buttonUpPin|buttonDownPin,GPIO_FALLING_EDGE);
-	GPIOIntEnable(GPIO_PORTA_BASE, buttonUpPin|buttonDownPin);
+	GPIOIntDisable(GPIO_PORTD_BASE, photosensorTop | photosensorBottom);
+	GPIOIntClear(GPIO_PORTD_BASE, photosensorTop | photosensorBottom);
+	GPIOIntRegister(GPIO_PORTD_BASE, portAISR);
+	GPIOIntTypeSet(GPIO_PORTD_BASE, photosensorTop | photosensorBottom,GPIO_BOTH_EDGES); //both edges pq detecto 1 y 0 dependiendo el color
+	GPIOIntEnable(GPIO_PORTD_BASE,photosensorTop | photosensorBottom);
 
-
-	// Interrupt setup 2
-	GPIOIntDisable(GPIO_PORTC_BASE, buttonDebouncePin);
-	GPIOIntClear(GPIO_PORTC_BASE, buttonDebouncePin);
-	GPIOIntRegister(GPIO_PORTC_BASE, portCISR);
-	GPIOIntTypeSet(GPIO_PORTC_BASE, buttonDebouncePin,GPIO_FALLING_EDGE);
-	GPIOIntEnable(GPIO_PORTC_BASE, buttonDebouncePin);
-
-
-	// Interrupt setup 3 PD2/PD3
-	GPIOIntDisable(GPIO_PORTD_BASE, buttonUpPin|buttonDownPin);
-	GPIOIntClear(GPIO_PORTD_BASE, buttonUpPin|buttonDownPin);
-	GPIOIntRegister(GPIO_PORTD_BASE, portDISR);
-	GPIOIntTypeSet(GPIO_PORTD_BASE, buttonUpPin|buttonDownPin,GPIO_FALLING_EDGE);
-	GPIOIntEnable(GPIO_PORTD_BASE, buttonUpPin|buttonDownPin);
 
 	FourBitInitialize();
 
 	sprintf(buffer, "%d", globalCounter);
-	updateSizeOfInteger();
-	displayLine(buffer,sizeOfInteger); //cast char
+	//updateSizeOfInteger();
+	//displayLine(buffer,sizeOfInteger); //cast char
+
+	currentState= (GPIOIntStatus(GPIO_PORTD_BASE, false)& (photosensorTop | photosensorBottom));
+	currentState = currentState<<2;
+	currentState = 0x0F & currentState;
+	globalCounter=-1;
 
 
 	while(1); //busyWait
@@ -178,57 +189,191 @@ void displayLine(char line[], int length){
 
 void globalCounterPlus(){
 
-	globalCounter++;
-	sprintf(buffer, "%d", globalCounter);
-	updateSizeOfInteger();
-	displayLine(buffer,sizeOfInteger);
+	if (globalCounter<16){
+		globalCounter++;
+	}
+	else{
+		globalCounter=0;
+	}
+	lineSwitcher();
 }
 
 void globalCounterLess(){
-
-	globalCounter--;
-	sprintf(buffer, "%d", globalCounter);
-	updateSizeOfInteger();
-	displayLine(buffer,sizeOfInteger);
+	if (globalCounter>-1){
+		globalCounter--;
+	}
+	else{
+		globalCounter=15;
+	}
+	lineSwitcher();
 }
 
 void portAISR(void){
-	GPIOIntDisable(GPIO_PORTA_BASE, buttonUpPin|buttonDownPin);
-	millisDelay(20);
-	GPIOIntEnable(GPIO_PORTA_BASE, buttonUpPin|buttonDownPin);
-	if(GPIOIntStatus(GPIO_PORTA_BASE, false) & buttonUpPin){
-		//displayLine("buttonUpPin",11);
-		globalCounterPlus();
-		GPIOIntClear(GPIO_PORTA_BASE, buttonUpPin);
-	}
-	else if(GPIOIntStatus(GPIO_PORTA_BASE, false) & buttonDownPin){
-			//displayLine("buttonDownPin",13);
-		globalCounterLess();
-		GPIOIntClear(GPIO_PORTA_BASE, buttonDownPin);
+	GPIOIntDisable(GPIO_PORTA_BASE, photosensorTop | photosensorBottom);
+	uint8_t tempVariable= (GPIOPinRead(GPIO_PORTA_BASE,(photosensorTop | photosensorBottom)));
+	//uint8_t tempVariableBottom= (GPIOIntStatus(GPIO_PORTA_BASE, false)& (photosensorBottom));
+	//uint8_t tempVariableTop= (GPIOIntStatus(GPIO_PORTA_BASE, false)& (photosensorTop));
+	//tempVariable= (photosensorTop | photosensorBottom) & tempVariable;
+	//uint32_t tempVariable2= GPIOIntStatus(GPIO_PORTA_BASE, false);
+	//currentState =  currentState << 2 ;
+	currentState = currentState<<2|tempVariable>>2;
+	currentState = 0x0F & currentState;
+	tableLookup();
+	//currentState = GPIOIntStatus(GPIO_PORTA_BASE, false) & photosensorTop | GPIOIntStatus(GPIO_PORTA_BASE, false) & photosensorBottom;
+	//printf("tempVariableTop: %d \n", tempVariableTop);
+	//printf("tempVariableBottom: %d \n", tempVariableBottom);
+	GPIOIntClear(GPIO_PORTA_BASE, photosensorTop | photosensorBottom);
+	GPIOIntEnable(GPIO_PORTA_BASE, photosensorTop | photosensorBottom);
 
-	}
+
+}
+
+void portDISR(void){
+	GPIOIntDisable(GPIO_PORTA_BASE, photosensorTop | photosensorBottom);
+	uint8_t tempVariable= (GPIOPinRead(GPIO_PORTA_BASE,(photosensorTop | photosensorBottom)));
+	//uint8_t tempVariableBottom= (GPIOIntStatus(GPIO_PORTA_BASE, false)& (photosensorBottom));
+	//uint8_t tempVariableTop= (GPIOIntStatus(GPIO_PORTA_BASE, false)& (photosensorTop));
+	//tempVariable= (photosensorTop | photosensorBottom) & tempVariable;
+	//uint32_t tempVariable2= GPIOIntStatus(GPIO_PORTA_BASE, false);
+	//currentState =  currentState << 2 ;
+	currentState = currentState<<2|tempVariable>>2;
+	currentState = 0x0F & currentState;
+	tableLookup();
+	//currentState = GPIOIntStatus(GPIO_PORTA_BASE, false) & photosensorTop | GPIOIntStatus(GPIO_PORTA_BASE, false) & photosensorBottom;
+	//printf("tempVariableTop: %d \n", tempVariableTop);
+	//printf("tempVariableBottom: %d \n", tempVariableBottom);
+	GPIOIntClear(GPIO_PORTA_BASE, photosensorTop | photosensorBottom);
+	GPIOIntEnable(GPIO_PORTA_BASE, photosensorTop | photosensorBottom);
+
+
 }
 
 void portCISR(void){
-	if(GPIOIntStatus(GPIO_PORTC_BASE, false) & buttonDebouncePin){
-		//displayLine("buttonUpPin",11);
-		globalCounterPlus();
-		GPIOIntClear(GPIO_PORTC_BASE, buttonDebouncePin);
+
+
+}
+
+void lineSwitcher(){
+	switch(globalCounter) {
+
+	case 0  :
+		displayLine(line0,lineLengthArray[globalCounter] );
+		break;
+
+	case 1  :
+	{
+		displayLine(line1,lineLengthArray[globalCounter] );
+	}
+	break;
+	case 2  :
+	{
+		displayLine(line2,lineLengthArray[globalCounter] );
+	}
+	break;
+	case 3  :
+	{
+		displayLine(line3,lineLengthArray[globalCounter] );
+	}
+	break;
+	case 4  :
+	{
+		displayLine(line4,lineLengthArray[globalCounter] );
+	}
+	break;
+	case 5  :
+	{
+		displayLine(line5,lineLengthArray[globalCounter] );
+	}
+	break;
+	case 6  :
+	{
+		displayLine(line6,lineLengthArray[globalCounter] );
+	}
+	break;
+	case 7  :
+	{
+		displayLine(line7,lineLengthArray[globalCounter] );
+	}
+	break;
+	case 8  :
+	{
+		displayLine(line8,lineLengthArray[globalCounter] );
+	}
+	break;
+	case 9  :
+	{
+		displayLine(line9,lineLengthArray[globalCounter] );
+	}
+	break;
+	case 10  :
+	{
+		displayLine(line10,lineLengthArray[globalCounter] );
+	}
+	break;
+	case 11  :
+	{
+		displayLine(line11,lineLengthArray[globalCounter] );
+	}
+	break;
+	case 12  :
+	{
+		displayLine(line12,lineLengthArray[globalCounter] );
+	}
+	break;
+	case 13  :
+	{
+		displayLine(line13,lineLengthArray[globalCounter] );
+	}
+	break;
+	case 14  :
+	{
+		displayLine(line14,lineLengthArray[globalCounter] );
+	}
+	break;
+	case 15  :
+	{
+		displayLine(line15,lineLengthArray[globalCounter] );
+	}
+	break;
 	}
 
 }
 
-void updateSizeOfInteger(){
- if (globalCounter <= -100 && globalCounter >-10000) sizeOfInteger = 4;
- if (globalCounter <= -10 && globalCounter >-100) sizeOfInteger = 3;
- if (globalCounter <= -1 && globalCounter >-10) sizeOfInteger = 2;
- if (globalCounter <= 9 && globalCounter >-1) sizeOfInteger = 1;
- if (globalCounter <= 99 && globalCounter >9) sizeOfInteger = 2;
- if (globalCounter <= 999 && globalCounter >99) sizeOfInteger = 3;
+void tableLookup(){
+	switch(currentState) {
 
-}
-
-
-void portDISR(void){
+	case 0  :{}
+	break;
+	case 1  : {globalCounterPlus();}
+	break;
+	case 2  :{globalCounterLess();}
+	break;
+	case 3  :{}
+	break;
+	case 4  :{globalCounterLess();}
+	break;
+	case 5  :{}
+	break;
+	case 6  :{}
+	break;
+	case 7  :{globalCounterPlus();}
+	break;
+	case 8  :{globalCounterPlus();}
+	break;
+	case 9  :{}
+	break;
+	case 10  :{}
+	break;
+	case 11  :{globalCounterLess();}
+	break;
+	case 12  :{}
+	break;
+	case 13  :{globalCounterLess();}
+	break;
+	case 14  :{globalCounterPlus();}
+	break;
+	case 15  :{}
+	break;
+	}
 
 }
